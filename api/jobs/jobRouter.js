bcee6c7da5b27ad0ce811bb8bba6584651218c3c
JonathanChen <jonny29009@yahoo.com>
2020-02-03 05:45:42 -0800

    create endpoints to be filled out

---
+++ api/jobs/jobRouter.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+//const {  } = require('../../data/helpers/index');
+//const {  } = require('../middlewares/index');
+
+// Returns all jobs, available to all users
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const authRouter = require('./auth/authRouter');
+const jobseekersRouter = require('./jobseekers/jobseekersRouter');
+const companiesRouter = require('./companies/companyRouter');
+const jobsRouter = require('./jobs/jobRouter');
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use('/api', authRouter);
+server.use('/api/jobseekers', jobseekersRouter);
+server.use('/api/companies', companiesRouter);
+server.use('/api/jobs', jobsRouter);
 
 server.get('/', (req, res) => {
     res.json({ message: 'Droom app backend up and running' })
