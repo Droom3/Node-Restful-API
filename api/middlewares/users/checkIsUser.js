e3b97e127457460e51c21461cc11c8aa9486c831
JonathanChen <jonny29009@yahoo.com>
2020-02-04 14:02:33 -0800

    logging message

--- api/middlewares/users/checkIsUser.js
+++ api/middlewares/users/checkIsUser.js
@@ -1,10 +1,10 @@
 function checkIsUser(req, res, next) {
     const user_type = parseInt(req.decoded_token.user_type);
-    if(parseInt(user_type)===1) {
+    if(user_type===1) {
         next();
     }
     else {
-        res.status(400).json({ message: 'you are not a user or jobseeker', stuff: parseInt(user_type) })
+        res.status(400).json({ message: 'you are not a user or jobseeker', stuff: user_type })
     }
 }
 
