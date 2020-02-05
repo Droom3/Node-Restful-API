f7ef890a5966dc7650fb9aa1a9e85079273d8e85
Mr. Jonah <jaitch@gmail.com>
2020-02-05 16:54:12 -0600

    Image upload route

---
+++ api/images/imagesRouter.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+
+const router = require('express').Router();
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+
+const ID = process.env.AWS_ID;
+const SECRET = process.env.AWS_SECRET;
+const BUCKET_NAME = 'droom3';
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: BUCKET_NAME,
+        metadata: function(req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function(req, file, cb) {
+            cb(null, `${file.originalname}`);
+        }
+    })
+})
+
+router.post('/', upload.array('photos', 3), function(req, res, next) {
+    //res.status(200).json({ success})
+    res.send(`Successfully uploaded ${req.files.length} files`);
+});
+
+module.exports = router;
+
+// const uploadFile = (fileName) => {
+//     const fileContent = fs.readFileSync(fileName);
+
+//     const params = {
+//         Bucket: BUCKET_NAME,
+//         Key: fileName,
+//         Body: fileContent
+//     }
+
+//     s3.upload(params, function(err, data){
+//         if (err) {
+//             throw err
+//         }
+//         console.log(`File uploaded successfully. ${data.Location}`);
+//     })
+// }
+
+// uploadFile('pink-scooter.jpg')
+
+// const s3 = new AWS.S3({
+//     accessKeyId: ID,
+//     secretAccessKey: SECRET
+// });
+
+// const params = {
+//     Bucket: BUCKET_NAME,
+//     CreateBucketConfiguration: {
+//         LocationConstraint: "us-west-1"
+//     }
+// }
+
+// s3.createBucket(params, function(err, data) {
+//     if (err) console.log(err, err.stack);
+//     else console.log('Bucket Created Successfully', data.Location);
+//})
+
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const authRouter = require('./auth/authRouter');
 const usersRouter = require('./users/usersRouter');
 const companiesRouter = require('./companies/companyRouter');
 const matchesRouter = require('./matches/matchesRouter')
+const imagesRouter = require('./images/imagesRouter');
 
 const server = express();
 server.use(helmet());
@@ -14,6 +15,7 @@ server.use('/api', authRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/companies', companiesRouter);
 server.use('/api/matches', matchesRouter);
+server.use('/api/images', imagesRouter);
 
 server.get('/', (req, res) => {
     res.json({ message: 'Droom app backend up and running' })
